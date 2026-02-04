a5fbef062caadc5b20645843c077daa6d63d9457
Shalini-jadala <jspforever143@gmail.com>
2026-02-04 12:24:38 +1100

    added basic layout of project

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+    return(
+        <div className="Weather">
+            <form>
+                <div className="row">
+                       <div className="col-8">
+                         <input type="search" 
+                         placeholder="Enter a city name.."
+                         className="long-button"/>
+                       </div>
+                       <div className="col-4">
+                           <input type="submit" 
+                            value="Search" 
+                            className="btn btn-primary" />
+                       </div>
+                </div>
+            </form>
+            <h1>Australia</h1>
+            <ul>
+                <li>Wednesday 07:00</li>
+                <li>Mostly Sunny</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg" 
+                    alt="Mostly Sunny" />
+                    25°C
+
+                </div>
+                <div className="col-6">
+                 <ul>
+                    <li>Precipitation: 25%</li>
+                    <li>Humidity: 75%</li>
+                    <li>Wind: 25km/hr</li>
+                </ul>   
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
