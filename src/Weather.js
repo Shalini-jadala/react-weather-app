6f98ea065de8010bcdc0e7b19471ccb8df118ca0
Shalini-jadala <jspforever143@gmail.com>
2026-02-10 13:42:04 +1100

    added API to forecast

--- src/Weather.js
+++ src/Weather.js
@@ -13,6 +13,7 @@ export default function Weather(props){
     function handleResponse(response){
     setWeatherData({
         ready: true,
+        coordinates: response.data.coord,
         temperature: response.data.temperature.current,
         humidity: response.data.temperature.humidity,
         description: response.data.condition.description,
@@ -63,7 +64,7 @@ export default function Weather(props){
                 </div>
             </form>
             <WeatherInfo data = {weatherData} />
-            <WeatherForecast />
+            <WeatherForecast coordinates={weatherData.coordinates}/>
             
         </div>
 
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,32 @@
 import React from "react";
 import WeatherIcons from "./WeatherIcons";
 import "./WeatherForecast.css";
+import axios  from "axios";
 
-export default function WeatherForecast(){
+export default function WeatherForecast(props){
+
+    
+
+
+    function handleResponse(response){
+        console.log(response.data);
+
+    }
+
+    let apiKey = "0a8c6e7ct4a98df4436abe8fo0c14f17";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=-${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+
+
+    axios(apiUrl).then(handleResponse);
+    
+    
+        
+    
     return(
+
+
     <div className="WeatherForecast">
         <div className="row">
             <div className="col">
@@ -20,6 +43,7 @@ export default function WeatherForecast(){
 
     </div>
     );
+
 
     
 }
